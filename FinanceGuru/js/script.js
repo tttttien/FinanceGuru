e8718a1cccbcee6b82be87207c81609b843f6506
Dac Le <lebalinh@MacBook-Pro-of-Le.local>
2024-04-16 21:16:07 +0700

    update course

--- FinanceGuru/js/script.js
+++ FinanceGuru/js/script.js
@@ -16,3 +16,15 @@ function closeSidebar(){
         sidebarOpen=false;
     }
 }
+
+/* Course */ 
+// JavaScript để điều khiển hiển thị/ẩn nội dung dropdown
+document.addEventListener("DOMContentLoaded", function() {
+    var dropdowns = document.getElementsByClassName("dropdown");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var dropdown = dropdowns[i];
+      dropdown.addEventListener("click", function() {
+        this.classList.toggle("open");
+      });
+    }
+  });
