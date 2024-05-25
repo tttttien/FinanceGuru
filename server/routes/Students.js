cd95732895ddcf08b232607919c292d80ad86df3
TUYETMAI\HP <duongmai6241@gmail.com>
2024-05-26 04:47:50 +0700

    get & post

---
+++ server/routes/Students.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const { Studentss } = require("../models");
+
+// Request to get all products
+router.get("/", async (req, res) => {
+  try {
+    const listOfProducts = await Products.findAll({
+      order: [["Num", "DESC"]], // Sort by Num column in descending order
+    });
+    res.json(listOfProducts);
+  } catch (error) {
+    console.error("Error fetching students:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+
+router.get("/:name", async (req, res) => {
+  try {
+    const students = await Students.findOne({
+      where: { Name: req.params.name },
+    });
+    if (!students) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+    res.json(students);
+  } catch (error) {
+    console.error("Error fetching Student by Name:", error);
+    console.log("500");
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+// Request to create a new student
+router.post("/", async (req, res) => {
+  const student = req.body;
+  await Students.create(student);
+  res.json(student);
+});
+
+module.exports = router;
