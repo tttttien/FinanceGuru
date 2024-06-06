ae839294d4a7a0cdb9e2952d394b2614ef0e6436
TUYETMAI\HP <duongmai6241@gmail.com>
2024-06-06 15:57:13 +0700

    login

--- server/routes/Employees.js
+++ server/routes/Employees.js
@@ -32,7 +32,6 @@ router.get("/:name", async (req, res) => {
     }
 });
 
-// Request to create a new employee
 router.post("/", async (req, res) => {
     try {
         const employee = req.body;
@@ -43,6 +42,37 @@ router.post("/", async (req, res) => {
         res.status(500).json({ error: "Server error" });
     }
 });
+
+// router.post("/", async (req, res) => {
+//     try {
+//         const {FullName, EmployeeEmail, EmployeeDOB, Password, Gender, Position, EmployeePhone, EmployeeAddress } = req.body;
+
+//         if (!Password || Password.trim() === "") {
+//             return res.status(400).json({ message: "Password cannot be empty" });
+//         }
+
+//         const hashedPassword = await bcrypt.hash(Password, 10);
+
+//         const user = {
+//             FullName,
+//             Gender,
+//             EmployeeDOB,
+//             EmployeeAddress,
+//             EmployeePhone,
+//             Position,
+//             EmployeeEmail,
+//             Password: hashedPassword,
+//         };
+
+//         userStore.set(EmployeeEmail, user);
+
+//         res.status(200).json({ message: "Employee created successfully" });
+//     } catch (error) {
+//         console.error("Error creating employee:", error);
+//         res.status(500).json({ error: "Server error" });
+//     }
+// });
+
 
 router.delete('/delete/:employeeId', async (req, res) => {
     try {
@@ -58,5 +88,19 @@ router.delete('/delete/:employeeId', async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 });
+router.post("/Login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await Employees.findOne({ where: { EmployeeEmail: email } });
 
+    if (!user) {
+        res.json({ error: "User Doesn't Exist" });
+    } else {
+        // **WARNING: This approach is not secure!**
+        if (password === user.Password) {
+            res.json({ message: "LOGGED IN!!!", user: user });
+        } else {
+            res.status(400).json({ error: "Wrong Username And Password Combination" });
+        }
+    }
+});
 module.exports = router;
